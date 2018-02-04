91de23bc099fa1360c149b4a1ed327373456a5f1
Vaibhavmulti <vaibhavmulti@gmail.com>
2018-02-04 23:14:15 +0530

    Week2

---
+++ Assets/XGame/GazableObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnGazeEnter(RaycastHit hit)
+    {
+        Debug.Log("GazeEntered"+hit.transform.name);
+    }
+
+    public void OnGazeStay(RaycastHit hit)
+    {
+        Debug.Log("GazeStay"+ hit.transform.name);
+    }
+
+    public void OnGazeLeave()
+    {
+        Debug.Log("GazeLeft");
+    }
+
+    public void OnButtonPress(RaycastHit hit)
+    {
+        Debug.Log("button pressed ");
+    }
+
+    public void OnButtonHold(RaycastHit hit)
+    {
+        Debug.Log("button Holded ");
+    }
+
+    public void OnButtonRelease(RaycastHit hit)
+    {
+        Debug.Log("button released ");
+    }
+}
---
+++ Assets/XGame/GazeInput.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInput : MonoBehaviour {
+    private GameObject previousGaze;
+    private GameObject currentSelected;
+    private RaycastHit lastHit;
+
+    public GameObject reticle;
+    public Color initialColor = Color.grey;
+    public Color colorChange = Color.green;
+	// Use this for initialization
+	void Start () {
+        ChangeColor(initialColor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HeadGaze();
+        CheckForInput(lastHit);
+	}
+
+    void HeadGaze()
+    {
+        Ray headGaze = new Ray(transform.position, transform.forward);
+        RaycastHit hitted;
+        Debug.DrawRay(transform.position,transform.forward*100);
+        if(Physics.Raycast(headGaze,out hitted))
+        {
+            GameObject currentGameObject= hitted.transform.gameObject;
+            lastHit = hitted;
+            if (currentGameObject.GetComponent<GazableObject>())
+            {
+                if (previousGaze == null)
+                {
+                    previousGaze = currentGameObject;
+                    ChangeColor(colorChange);
+                    currentGameObject.GetComponent<GazableObject>().OnGazeEnter(hitted);
+                }
+                if (previousGaze == currentGameObject)
+                {
+                    currentGameObject.GetComponent<GazableObject>().OnGazeStay(hitted);
+                }
+                if(previousGaze!=currentGameObject)
+                {
+                    previousGaze.GetComponent<GazableObject>().OnGazeLeave();
+                    previousGaze = currentGameObject;
+                    currentGameObject.GetComponent<GazableObject>().OnGazeEnter(hitted);
+                }
+            }
+            else
+            {
+                if (previousGaze != null)
+                    previousGaze.GetComponent<GazableObject>().OnGazeLeave();
+                ClearGaze();
+            }  
+        }
+        else
+            ClearGaze();
+    }
+
+    void CheckForInput(RaycastHit ray)
+    {
+        if(Input.GetButtonDown("Fire1") && previousGaze!=null)
+        {
+            previousGaze.GetComponent<GazableObject>().OnButtonPress(ray);
+            currentSelected = previousGaze;
+        }
+        if (Input.GetButton("Fire1") && currentSelected != null)
+        {
+            currentSelected.GetComponent<GazableObject>().OnButtonHold(ray);
+        }
+        if (Input.GetButtonUp("Fire1") && currentSelected != null)
+        {
+            currentSelected.GetComponent<GazableObject>().OnButtonRelease(ray);
+ 
+        }
+
+    }
+
+    void ClearGaze()
+    {
+        previousGaze = null;
+        ChangeColor(initialColor);
+    }
+
+    void ChangeColor(Color colour)
+    {
+        reticle.GetComponent<MeshRenderer>().material.color = colour;
+    }
+}
